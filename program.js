88933d8fd21168d82020fa1fb78cd185cfab6124
Avinash Kumar <avinashtelcom@gmail.com>
2016-08-29 09:55:55 +0000

    Step 2

--- program.js
+++ program.js
@@ -1,8 +1,7 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 
-app.get('/home',function(req, res) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World!");
-});
+app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+
 app.listen(process.argv[2]);
