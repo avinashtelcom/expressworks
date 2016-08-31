5c30ef696a52616122e4e076eb9de72cb3c8dea0
Avinash Kumar <avinashtelcom@gmail.com>
2016-08-31 05:44:36 +0000

    Step 8

--- program.js
+++ program.js
@@ -1,9 +1,15 @@
 var express = require("express");
-
+var fs = require("fs");
 var app = express();
 
-app.get('/search', function(req, res) {
-    res.send(req.query);
+app.get('/books', function(req, res) {
+    fs.readFile(process.argv[3], function(err, data) {
+        if(err) {
+            console.error("Error");
+        }
+        var jsonObj = JSON.parse(data.toString());
+        res.json(jsonObj);
+    });
 });
 
 app.listen(process.argv[2]);
